c2f0fee9a5906551861e7a31bdbcfc7cb8154a8b
Deepthi45805 <24311a05kh@cse.sreenidhi.edu.in>
2026-06-04 19:01:58 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Recruiter AI Dashboard")
+
+st.header("Candidate Information")
+
+st.write("Name: Deepthi")
+st.write("Skills: Python, SQL, Machine Learning")
+st.write("Experience: Fresher")
+
+st.header("Match Results")
+
+st.write("Matched Skills: Python, SQL")
+st.write("Match Percentage: 66.67%")
