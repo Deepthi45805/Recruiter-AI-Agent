7918e4bbffe884b93ac89d3285d1d648a7841bfa
Deepthi45805 <24311a05kh@cse.sreenidhi.edu.in>
2026-06-04 19:10:46 +0530

    Create interview_scheduler.py

---
+++ interview_scheduler.py
@@ -0,0 +1,8 @@
+candidate_name = input("Enter candidate name: ")
+interview_date = input("Enter interview date: ")
+interview_time = input("Enter interview time: ")
+
+print("\nInterview Scheduled")
+print("Candidate:", candidate_name)
+print("Date:", interview_date)
+print("Time:", interview_time)
